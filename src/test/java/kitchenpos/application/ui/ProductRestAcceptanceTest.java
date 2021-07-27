a51b49040da7213aaaa53986de9ad1508ed81d92
sanggu <tkdrn8578@naver.com>
2021-07-27 23:49:04 +0900

    refactor: 가독성을 위한 method extract 진행

--- src/test/java/kitchenpos/application/ui/ProductRestAcceptanceTest.java
+++ src/test/java/kitchenpos/application/ui/ProductRestAcceptanceTest.java
@@ -21,17 +21,29 @@ public class ProductRestAcceptanceTest extends AcceptanceTest {
 	@Test
 	void createProduct() {
 		//given
-		ProductRequest params = new ProductRequest("테스트 상품", BigDecimal.valueOf(20000));
+		ProductRequest params = 품목을_생성한다();
 
 		// when
-		ExtractableResponse<Response> response = RestAssured
-		        .given().log().all()
-		        .body(params)
-		        .contentType(MediaType.APPLICATION_JSON_VALUE)
-		        .when().post("/api/products")
-		        .then().log().all().extract();
+		ExtractableResponse<Response> response = 품목_생성을_요청한다(params);
 
 		// then
+		품목이_생성됨(response);
+	}
+
+	private ProductRequest 품목을_생성한다() {
+		return new ProductRequest("테스트 상품", BigDecimal.valueOf(20000));
+	}
+
+	private ExtractableResponse<Response> 품목_생성을_요청한다(ProductRequest params) {
+		return RestAssured
+					.given().log().all()
+					.body(params)
+					.contentType(MediaType.APPLICATION_JSON_VALUE)
+					.when().post("/api/products")
+					.then().log().all().extract();
+	}
+
+	private void 품목이_생성됨(ExtractableResponse<Response> response) {
 		Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 	}
 }
