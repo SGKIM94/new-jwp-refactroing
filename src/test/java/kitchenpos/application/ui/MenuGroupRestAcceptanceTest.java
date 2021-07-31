23fcbf5e03c14c79c85cf38d77a3b13a37f5fd2b
sanggu <tkdrn8578@naver.com>
2021-07-31 23:48:38 +0900

    refactor: MenuGroupRequest 추가에 따른 수정

--- src/test/java/kitchenpos/application/ui/MenuGroupRestAcceptanceTest.java
+++ src/test/java/kitchenpos/application/ui/MenuGroupRestAcceptanceTest.java
@@ -4,14 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.AcceptanceTest;
+import kitchenpos.dto.MenuGroupRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @DisplayName("메뉴 그룹 인수테스트")
 public class MenuGroupRestAcceptanceTest extends AcceptanceTest {
@@ -19,12 +17,11 @@ public class MenuGroupRestAcceptanceTest extends AcceptanceTest {
 	@Test
 	void createMenuGroup() {
 		// when
-		Map<String, String> params = new HashMap<>();
-		params.put("name", "한식");
+		MenuGroupRequest request = new MenuGroupRequest("한식");
 
 		ExtractableResponse<Response> response = RestAssured
 		        .given().log().all()
-		        .body(params)
+		        .body(request)
 		        .contentType(MediaType.APPLICATION_JSON_VALUE)
 		        .when().post("/api/menu-groups")
 		        .then().log().all().extract();
