b13617de56ff146c1f4b751a2cdde28c77a0486c
sanggu <tkdrn8578@naver.com>
2021-08-01 22:56:28 +0900

    feat: list의 읃답값을 받아 MenuGroupsResponse로 변환하는 of 메서드 추가

--- src/main/java/kitchenpos/dto/MenuGroupsResponse.java
+++ src/main/java/kitchenpos/dto/MenuGroupsResponse.java
@@ -1,6 +1,9 @@
 package kitchenpos.dto;
 
+import kitchenpos.domain.MenuGroup;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MenuGroupsResponse {
 
@@ -11,6 +14,14 @@ public class MenuGroupsResponse {
 
 	public MenuGroupsResponse(List<MenuGroupResponse> menuGroups) {
 		this.menuGroups = menuGroups;
+	}
+
+	public static MenuGroupsResponse of(List<MenuGroup> menuGroups) {
+		List<MenuGroupResponse> responses = menuGroups.stream()
+				.map(MenuGroupResponse::of)
+				.collect(Collectors.toList());
+
+		return new MenuGroupsResponse(responses);
 	}
 
 	public List<MenuGroupResponse> getMenuGroups() {
