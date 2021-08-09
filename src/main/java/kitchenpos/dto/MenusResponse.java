be4aa2f8916cd8cd09d00698be6cd2de15dd6fa4
sgkim94 <tkdrn8578@naver.com>
2021-08-09 23:15:52 +0900

    feat: Menu list를 조회했을 때에 대한 응답값을 전달하기 위한 dto 추가

---
+++ src/main/java/kitchenpos/dto/MenusResponse.java
@@ -0,0 +1,15 @@
+package kitchenpos.dto;
+
+import java.util.List;
+
+public class MenusResponse {
+	private List<MenuResponse> menus;
+
+	public MenusResponse(List<MenuResponse> menus) {
+		this.menus = menus;
+	}
+
+	public List<MenuResponse> getMenus() {
+		return menus;
+	}
+}
