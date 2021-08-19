ae0712b72a0973722a6155e95984fd0ba154a864
sgkim94 <tkdrn8578@naver.com>
2021-08-19 23:54:37 +0900

    refactor: menuId, productId 가 아닌 Menu, Product를 리턴하도록 변경 및 of 함수 생성

--- src/main/java/kitchenpos/menu/dto/MenuProductResponse.java
+++ src/main/java/kitchenpos/menu/dto/MenuProductResponse.java
@@ -1,28 +1,36 @@
 package kitchenpos.menu.dto;
 
-public class MenuProductResponse {
-	private Long seq;
-	private Long menuId;
-	private Long productId;
-	private long quantity;
+import kitchenpos.menu.domain.MenuProduct;
+import kitchenpos.product.dto.ProductResponse;
 
-	public MenuProductResponse(Long seq, Long menuId, Long productId, long quantity) {
+public class MenuProductResponse {
+	private final Long seq;
+	private final MenuResponse menu;
+	private final ProductResponse product;
+	private final long quantity;
+
+	public MenuProductResponse(Long seq, MenuResponse menu, ProductResponse product, long quantity) {
 		this.seq = seq;
-		this.menuId = menuId;
-		this.productId = productId;
+		this.menu = menu;
+		this.product = product;
 		this.quantity = quantity;
+	}
+
+	public static MenuProductResponse of(MenuProduct menuProduct) {
+		return new MenuProductResponse(menuProduct.getSeq(), MenuResponse.of(menuProduct.getMenu()),
+				ProductResponse.of(menuProduct.getProduct()), menuProduct.getQuantity());
 	}
 
 	public Long getSeq() {
 		return seq;
 	}
 
-	public Long getMenuId() {
-		return menuId;
+	public MenuResponse getMenu() {
+		return menu;
 	}
 
-	public Long getProductId() {
-		return productId;
+	public ProductResponse getProduct() {
+		return product;
 	}
 
 	public long getQuantity() {
