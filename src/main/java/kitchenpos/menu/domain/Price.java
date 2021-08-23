d8489ccf8e327579a19fca20ca949297a84bd382
sgkim94 <tkdrn8578@naver.com>
2021-08-23 23:59:44 +0900

    refactor: 매직넘버인 0을 상수로 추출

--- src/main/java/kitchenpos/menu/domain/Price.java
+++ src/main/java/kitchenpos/menu/domain/Price.java
@@ -1,9 +1,10 @@
 package kitchenpos.menu.domain;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 public class Price {
+	public static final int MINIMUM = 0;
+
 	private final BigDecimal price;
 
 	public Price(int price) {
@@ -12,19 +13,19 @@ public class Price {
 	}
 
 	private void validatePrice(int price) {
-		if (price < 0) {
+		if (price < MINIMUM) {
 			throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다. : " + price);
 		}
 	}
 
 	public void validatePrice() {
-		if (price.compareTo(BigDecimal.ZERO) < 0) {
+		if (price.compareTo(BigDecimal.ZERO) < MINIMUM) {
 			throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다. : " + price);
 		}
 	}
 
 	public void validateSum(BigDecimal sum) {
-		if (price.compareTo(sum) > 0) {
+		if (price.compareTo(sum) > MINIMUM) {
 			throw new IllegalArgumentException();
 		}
 	}
