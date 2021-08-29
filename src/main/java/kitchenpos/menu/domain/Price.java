fa8fd84e360a9cc6fa8806f88c3871d12ca355ec
sanggu <tkdrn8578@naver.com>
2021-08-29 17:06:59 +0900

    refactor: price의 타입을 long으로 변경 및 더하기 및 곱하기에 대한 함수와 테스트 코드 추가

--- src/main/java/kitchenpos/menu/domain/Price.java
+++ src/main/java/kitchenpos/menu/domain/Price.java
@@ -1,36 +1,60 @@
 package kitchenpos.menu.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Price {
 	public static final int MINIMUM = 0;
+	public static final Price ZERO = new Price(0);
 
-	private final BigDecimal price;
+	private final long price;
 
-	public Price(int price) {
+	public Price(long price) {
 		validatePrice(price);
-		this.price = BigDecimal.valueOf(price);
+		this.price = price;
 	}
 
-	private void validatePrice(int price) {
+	public Price(BigDecimal price) {
+		long priceToLong = price.longValue();
+
+		validatePrice(priceToLong);
+		this.price = priceToLong;
+	}
+
+	private void validatePrice(long price) {
 		if (price < MINIMUM) {
 			throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다. : " + price);
 		}
 	}
 
 	public void validatePrice() {
-		if (price.compareTo(BigDecimal.ZERO) < MINIMUM) {
+		if (price < MINIMUM) {
 			throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다. : " + price);
 		}
 	}
 
-	public void validateSum(BigDecimal sum) {
-		if (price.compareTo(sum) > MINIMUM) {
-			throw new IllegalArgumentException();
-		}
+	public long multiply(long quantity) {
+		return price * quantity;
 	}
 
-	public BigDecimal getPrice() {
+	public Price add(long sum) {
+		return new Price(price + sum);
+	}
+
+	public long getPrice() {
 		return price;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Price price1 = (Price) o;
+		return price == price1.price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(price);
 	}
 }
--- src/test/java/kitchenpos/menu/domain/PriceTest.java
+++ src/test/java/kitchenpos/menu/domain/PriceTest.java
@@ -3,15 +3,36 @@ package kitchenpos.menu.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PriceTest {
 
-	@DisplayName("금액이 0보다 작은경우 예외처리한다")
+	@DisplayName("금액이 0보다 작은경우 예외처리한다.")
 	@Test
 	void validatePriceWithNull() {
 		assertThrows(IllegalArgumentException.class,
 				() -> new Price(-20)
 		);
+	}
+
+	@DisplayName("금액을 수량에 따라 곱한다.")
+	@Test
+	void multiply_test() {
+		Price price = new Price(2000);
+
+		long result = price.multiply(4L);
+
+		assertThat(result).isEqualTo(8000);
+	}
+
+	@DisplayName("금액을 더한다.")
+	@Test
+	void sum_test() {
+		Price price = new Price(2000);
+
+		Price result = price.add(3000);
+
+		assertThat(result).isEqualTo(new Price(5000));
 	}
 }
