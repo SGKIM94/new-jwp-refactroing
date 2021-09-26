32adc656a0281c926b333ddd5c641b7ac396c6e3
sgkim94 <tkdrn8578@naver.com>
2021-09-26 23:52:52 +0900

    refactor: 불필요한 import 제거

--- src/main/java/kitchenpos/order/ui/TableRestController.java
+++ src/main/java/kitchenpos/order/ui/TableRestController.java
@@ -2,14 +2,18 @@ package kitchenpos.order.ui;
 
 import kitchenpos.order.application.TableService;
 import kitchenpos.order.domain.OrderTable;
-import kitchenpos.order.dto.OrderTableResponse;
 import kitchenpos.order.dto.OrderTableRequest;
+import kitchenpos.order.dto.OrderTableResponse;
 import kitchenpos.order.dto.OrderTablesResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 public class TableRestController {
