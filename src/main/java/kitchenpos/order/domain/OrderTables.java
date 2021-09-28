ef5768f4875b6d65330e99a6d13fc1703bb972fc
sgkim94 <tkdrn8578@naver.com>
2021-09-28 23:38:06 +0900

    refactor: orderTables의 empty를 검사하는 로직을 생성자로 이동

--- src/main/java/kitchenpos/order/domain/OrderTables.java
+++ src/main/java/kitchenpos/order/domain/OrderTables.java
@@ -1,5 +1,7 @@
 package kitchenpos.order.domain;
 
+import org.springframework.util.CollectionUtils;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,10 +9,17 @@ public class OrderTables {
 	private List<OrderTable> orderTables;
 
 	public OrderTables(List<OrderTable> orderTables) {
+		validateOrderTables(orderTables);
 		this.orderTables = orderTables;
 	}
 
-	List<Long> extractOrderTableIds(List<OrderTable> orderTables) {
+	private void validateOrderTables(List<OrderTable> orderTables) {
+		if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < 2) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public List<Long> extractOrderTableIds() {
 		return orderTables.stream()
 				.map(OrderTable::getId)
 				.collect(Collectors.toList());
