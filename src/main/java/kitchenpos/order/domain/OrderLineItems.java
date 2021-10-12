aa8f49686f5f213e4fc0c58cd392e2639cc7d1cf
sgkim94 <tkdrn8578@naver.com>
2021-10-12 23:27:24 +0900

    refactor: extractMenuIdsByOrderLineItems 을 OrderLineItems로 책임 이동

--- src/main/java/kitchenpos/order/domain/OrderLineItems.java
+++ src/main/java/kitchenpos/order/domain/OrderLineItems.java
@@ -1,12 +1,20 @@
 package kitchenpos.order.domain;
 
+import org.springframework.util.CollectionUtils;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderLineItems {
-	private List<OrderLineItem> orderLineItems;
+	private final List<OrderLineItem> orderLineItems;
 
 	public OrderLineItems(List<OrderLineItem> orderLineItems) {
 		this.orderLineItems = orderLineItems;
+	}
+	public List<Long> extractMenuIdsByOrderLineItems() {
+		return orderLineItems.stream()
+				.map(OrderLineItem::getMenuId)
+				.collect(Collectors.toList());
 	}
 
 	public List<OrderLineItem> getOrderLineItems() {
