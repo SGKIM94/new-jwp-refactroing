5d0439a66575e7f51e4ab33fb2cc8b06b752ec4c
sgkim94 <tkdrn8578@naver.com>
2021-11-14 21:59:33 +0900

    refactor: 불필요한 setter 제거

--- src/main/java/kitchenpos/menu/domain/Menu.java
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -30,28 +30,12 @@ public class Menu {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public Price getPrice() {
         return price;
-    }
-
-    public void setPrice(Price price) {
-        this.price = price;
-    }
-
-    public void setMenuGroup(MenuGroup menuGroup) {
-        this.menuGroup = menuGroup;
     }
 
     public MenuProducts getMenuProducts() {
