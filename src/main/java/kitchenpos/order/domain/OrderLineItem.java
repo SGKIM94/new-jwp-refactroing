63e151efec8612a69960b70016f2315670dda75f
sgkim94 <tkdrn8578@naver.com>
2021-12-14 23:54:03 +0900

    refactor: set를 제거하기 위한 생성자를 추가하여 변경

--- src/main/java/kitchenpos/order/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/order/domain/OrderLineItem.java
@@ -6,6 +6,16 @@ public class OrderLineItem {
     private Long menuId;
     private long quantity;
 
+    public OrderLineItem() {
+    }
+
+    public OrderLineItem(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
     public void setSeq(final Long seq) {
         this.seq = seq;
     }
