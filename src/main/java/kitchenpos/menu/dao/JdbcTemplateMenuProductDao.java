63a8f100895eb4413f0a6d1acb303e275c186527
sgkim94 <tkdrn8578@naver.com>
2021-12-15 23:17:16 +0900

    refactor: 가독성을 위한 바로 리턴하는 형태로 변경

--- src/main/java/kitchenpos/menu/dao/JdbcTemplateMenuProductDao.java
+++ src/main/java/kitchenpos/menu/dao/JdbcTemplateMenuProductDao.java
@@ -71,12 +71,11 @@ public class JdbcTemplateMenuProductDao implements MenuProductDao {
     }
 
     private MenuProduct toEntity(final ResultSet resultSet) throws SQLException {
-        final MenuProduct entity = new MenuProduct(
+        return new MenuProduct(
                 resultSet.getLong(KEY_COLUMN_NAME),
                 (Menu) resultSet.getObject("menu"),
                 (Product) resultSet.getObject("product_id"),
                 resultSet.getLong("quantity")
                 );
-        return entity;
     }
 }
