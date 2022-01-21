802237cf2040ec46f4f26c33ebf78fa131a9ce88
sgkim94 <tkdrn8578@naver.com>
2022-01-21 22:38:25 +0900

    feat: baseEntity에 id 추가

--- src/main/java/kitchenpos/order/domain/BaseEntity.java
+++ src/main/java/kitchenpos/order/domain/BaseEntity.java
@@ -3,6 +3,7 @@ package kitchenpos.order.domain;
 import java.time.LocalDateTime;
 
 public class BaseEntity {
+	private Long id;
 	private LocalDateTime createdDate;
 	private LocalDateTime updatedDate;
 
@@ -13,9 +14,18 @@ public class BaseEntity {
 		this.createdDate = createdDate;
 	}
 
+	public BaseEntity(Long id, LocalDateTime createdDate) {
+		this.id = id;
+		this.createdDate = createdDate;
+	}
+
 	public BaseEntity(LocalDateTime createdDate, LocalDateTime updatedDate) {
 		this.createdDate = createdDate;
 		this.updatedDate = updatedDate;
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 	public LocalDateTime getCreatedDate() {
